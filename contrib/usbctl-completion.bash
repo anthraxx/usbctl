e1e74239262dc4cc07c2b3156141e5088236b11b
kpcyrd <git@rxv.cc>
2018-04-27 03:11:22 +0200

    completions: add simple completions

---
+++ contrib/usbctl-completion.bash
@@ -0,0 +1,8 @@
+_usbctl_completions() {
+    if [ "${#COMP_WORDS[@]}" != "2" ]; then
+        return
+    fi
+
+    COMPREPLY=($(compgen -W "protect unprotect enable disable on off temporary check status" "${COMP_WORDS[1]}"))
+}
+complete -F _usbctl_completions usbctl
